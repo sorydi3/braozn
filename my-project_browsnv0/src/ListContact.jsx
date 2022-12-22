242ab3fc5bf6ff1d2432ff8556f2f946dbce68fa
ibradi3 <ibrahimasory22@hotmail.com>
2022-12-22 17:11:01 +0100

    contact list done

--- my-project_browsnv0/src/ListContact.jsx
+++ my-project_browsnv0/src/ListContact.jsx
@@ -4,10 +4,24 @@ import React from "react";
 export default function ListContact({ contacts, onEdit, onDelete }) {
     console.log(contacts);
     return(
-        <ol className="contact-list selection:flex flex-col justify-center items-center">
-            {contacts.map((contact) => 
-                (<li key={contact.id} className="contact-list-item flex flex-row justify-center items-center">{contact.name}</li>)
-            )}
-        </ol>
+        <div className="contact-list selection:flex flex-col justify-center items-center" >
+            <ol className=" px-10 py-10">
+                {contacts.map((contact) => 
+                    (<li key={contact.id} className="h-20  relative my-5  bg-slate-200 rounded-lg shadow-lg">
+                        
+                        <div className="bg-['url(../public/logo192.png)'] bg-fixed left-0 inset-y-0 bg-cover h-15 w-16 rounded bg-green-300 m-2 absolute"></div>
+
+                        <div className=" flex flex-row absolute left-20 inset-y-4 ml-5  w-20 h-13 ">
+                            <div className="contact-list-item-info ">@{contact.name}</div>
+
+                            <div className="contact-list-item-info ">{contact.handle}</div>
+                        </div>
+                        <button className="right-0 absolute mx-5 inset-y-5">Remove</button>
+                        
+                    </li>)
+                )}
+            </ol>
+        </div>
+        
     );
 }
