03e0b18f3f4e8ccd8790b7ced8e571b3c14dc2af
ibradi3 <ibrahimasory22@hotmail.com>
2022-12-24 16:28:24 +0100

    scale up list item when hovered

--- my-project_browsnv0/src/ListContact.jsx
+++ my-project_browsnv0/src/ListContact.jsx
@@ -11,7 +11,7 @@ export default function ListContact({ contacts, onEdit, onDelete }) {
         <div className="contact-list selection:flex flex-col flex-grow justify-center items-center" >
             <ol className=" px-10 py-10">
                 {contacts.map((contact) => 
-                    (<li key={contact.id} className="h-20  relative my-5 divide-x divide-solid  bg-slate-200 rounded-lg shadow-lg">
+                    (<li key={contact.id} className="h-20  relative my-5 divide-x divide-solid  bg-slate-200 rounded-lg shadow-lg hover:scale-105">
                         
                         <div className="bg-hero left-0 inset-y-0 bg-cover h-15 w-16 rounded-full bg-green-300 m-2 absolute"></div>
 
