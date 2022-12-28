a8b7285d2300226ed294432faa190434e270df22
ibradi3 <ibrahimasory22@hotmail.com>
2022-12-28 15:20:51 +0100

    code refactoring

--- my-project_browsnv0/src/Contact.js
+++ my-project_browsnv0/src/Contact.js
@@ -4,27 +4,6 @@ import ListContact from "./ListContact";
 import SearchComponent from "./SearchComponent";
 import DetailsModal from "./DetailsModal";
 
-const CContacts = [
-  {
-    id: "karen",
-    name: "Karen Isgrigg",
-    handle: "karen_isgrigg",
-    avatarURL: "http://localhost:5001/karen.jpg",
-  },
-  {
-    id: "richard",
-    name: "Richard Kalehoff",
-    handle: "richardkalehoff",
-    avatarURL: "http://localhost:5001/richard.jpg",
-  },
-  {
-    id: "tyler",
-    name: "Tyler McGinnis",
-    handle: "tylermcginnis",
-    avatarURL: "http://localhost:5001/tyler.jpg",
-  },
-];
-
 function handleOnOpen(setVisible) {
   setVisible(true);
 }
@@ -33,7 +12,6 @@ function SearchFilter(setContacts, search, contacts, setFiltered) {
   if (search === "") {
     setContacts(contacts);
   } else {
-    console.log(search);
     let filteredContacts = contacts.filter((contact) => {
       return contact.name.toLowerCase().includes(search.toLowerCase());
     });
@@ -47,7 +25,7 @@ export default function Contact() {
   const [search, setSearch] = useState("");
   const handleOnClose = () => setVisible(false);
   const handleOnOpen = () => setVisible(true);
-  const [contacts, setContacts] = useState(CContacts);
+  const [contacts, setContacts] = useState(Contacts);
   const [filtered, setFiltered] = useState([]);
   useEffect(() => {
     SearchFilter(setContacts, search, contacts, setFiltered);
