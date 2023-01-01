2923b336ab4889640babc23baa43dfff67caec69
ibradi3 <ibrahimasory22@hotmail.com>
2023-01-01 23:55:00 +0100

    centering logos using grid

---
+++ my-project_browsnv0/src/CenterLogos.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function CenteringLogos() {
+    return (<div className='grid grid-cols-[repeat(4,auto)] gap-12 justify-between justify-items-center content-center'>
+
+        <img src="https://res.cloudinary.com/thirus/image/upload/v1634556259/logos/logoipsum-5_wqq52e.svg" alt="" />
+        <img src="https://res.cloudinary.com/thirus/image/upload/v1634556262/logos/logoipsum-8_euk84k.svg" alt="" />
+        <img src="https://res.cloudinary.com/thirus/image/upload/v1634556259/logos/logoipsum-6_jie3be.svg" alt="" />
+        <img src="https://res.cloudinary.com/thirus/image/upload/v1634556262/logos/logoipsum-9_hqrvx7.svg" alt="" />
+        <img src="https://res.cloudinary.com/thirus/image/upload/v1634564578/logos/logoipsum-13_zvuric.svg" alt="" />
+        <img src="https://res.cloudinary.com/thirus/image/upload/v1634556261/logos/logoipsum-7_vuiq6n.svg" alt="" />
+        <img src="https://res.cloudinary.com/thirus/image/upload/v1634564391/logos/logoipsum-11_bhrqq2.svg" alt="" />
+        <img src="https://res.cloudinary.com/thirus/image/upload/v1634556262/logos/logoipsum-10_pbu8et.svg" alt="" />
+
+    </div>);
+}
+
+export default CenteringLogos;
--- my-project_browsnv0/src/MainContent.jsx
+++ my-project_browsnv0/src/MainContent.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import CenteringLogos from "./CenterLogos";
+
 export default function MainContent({ children }) {
   return (
     <div className="sidebar flex flex-col justify-center p-5 font-bold text-2xl text-white">
-        <h1 className="text-black m-auto"> Main content here!</h1>
-
-        <div className="h-1 rounded-lg w-60 m-auto bg-slate-400"></div>
-        <div className="h-1 rounded-lg w-60 m-auto bg-slate-400"></div>
-        <div className="h-1 rounded-lg w-60 m-auto bg-slate-400"></div>
-        <div className="h-1 rounded-lg w-60 m-auto bg-slate-400"></div>
-        <div className="h-1 rounded-lg w-60 m-auto bg-slate-400"></div>
-
-        <div className="h-1 rounded-lg w-60 m-auto bg-slate-400"></div>
-        <div className="h-1 rounded-lg w-60 m-auto bg-slate-400"></div>
+        <CenteringLogos/>
     </div>
   );
 }
