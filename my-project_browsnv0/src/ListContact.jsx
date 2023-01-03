191a85566c5a39c9328add0ffa12ca604c8ea7a6
ibradi3 <ibrahimasory22@hotmail.com>
2023-01-03 18:03:43 +0100

    ui fix

--- my-project_browsnv0/src/ListContact.jsx
+++ my-project_browsnv0/src/ListContact.jsx
@@ -12,18 +12,21 @@ export default function ListContact({contacts,onOpenModal,onRemoveContact}) {
         <div className="contact-list selection:flex flex-col flex-grow justify-center items-center" >
             <ol className=" px-10 ">
                 {contacts.map((contact,keys) => 
-                    (<li key={keys} className=" grid grid-cols-[30%,1fr]  my-3  bg-slate-200 rounded-lg shadow-lg hover:scale-105 items-center px-2">
-                        <div className="flex items-center">
-                                <div className="bg-hero flex-shrink-0 bg-cover h-16 w-16 rounded-full bg-green-300 m-2 "></div>
+                    (<li key={keys} className=" grid grid-cols-[auto,auto]  my-3  bg-slate-200 rounded-lg shadow-lg hover:scale-105 items-center px-2">
+                      
+                       <div className="flex items-center">
+                            <div className="bg-hero flex-shrink-0 bg-cover h-16 w-16 rounded-full bg-green-300 m-2 "></div>
 
-                                <div className=" w-1 ml-4 h-13 my-3  bg-amber-300 rounded-lg"></div>
+                            <div className=" w-1 ml-4 h-13 my-3  bg-amber-300 rounded-lg"></div>
 
-                                <div className="grid divide-x-2 divide-red-400 justify-end ">
-                                    <div className="contact-list-item-info sm:text-ellipsis ">{contact.name}</div>
+                            
 
-                                    <div className="contact-list-item-info px-2 ml-2 ">{contact.handle}</div>
-                                </div>
-                        </div>
+                            <div className="grid divide-x-2 divide-red-400 justify-end ">
+                                <div className="contact-list-item-info sm:text-ellipsis ">{contact.name}</div>
+
+                                <div className="contact-list-item-info px-2 ml-2 ">{contact.handle}</div>
+                            </div>
+                       </div>
                         <button className="ml-auto h-10 w-20 bg-red-400 hover:bg-red-700 hover:text-white   rounded-lg text-gray-900 " onClick={ () => onRemoveContact(contact.id)} >Remove</button>
                     </li>)
                 )}
